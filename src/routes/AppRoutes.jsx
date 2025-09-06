74785c918d214086fe41ad31b35f12befbcc6790
raushankumar <raushanm143@gmail.com>
2025-09-06 17:26:54 +0530

    changes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -138,9 +138,9 @@ export default function AppRoutes() {
               <Route path="customer/purchase" element={<MyPurchase />} />
               <Route path="customer/fund-request" element={<FundRequest />} />
               <Route path="customer/sale" element={<MySale />} />
-              <Route path="customer/notification" element={<Notification />} />
+         
               <Route path="customer/profile" element={<ProfilePage />} />
-              <Route path="customer/banks" element={<Banks />} />
+        
               <Route
                 path="customer/*"
                 element={<Navigate to="/customer/dashboard" replace />}
